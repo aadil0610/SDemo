579a976855d06257b4848dc35b1798536af13a21
shen <624354752@qq.com>
2016-08-23 22:36:31 +0800

    frist add splash activity

---
+++ app/src/main/java/com/isme/shen/sdemo/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.isme.shen.sdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.WindowDecorActionBar;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by shen on 2016/8/23.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        
+        init();
+    }
+
+    private void init() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        SplashActivity.this.startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                        overridePendingTransition(-1,-1);
+                    }
+                });
+            }
+        },3000);
+    }
+}
